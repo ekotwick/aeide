98163dc5a98edc8663a0c836d4734a24090557b1
ekotwick <edmond.kotwick@gmail.com>
2017-04-29 18:13:59 -0400

    material UI basics

--- app/main.jsx
+++ app/main.jsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import {render} from 'react-dom';
 import { Provider } from 'react-redux';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 import store from './store';
 import Routes from './routes';
