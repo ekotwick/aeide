54d4b383e2196f07d62653fd8a711de5550a62b0
ekotwick <edmond.kotwick@gmail.com>
2017-04-29 15:02:39 -0400

    closes #21

--- app/components/TextOutput.js
+++ app/components/TextOutput.js
@@ -3,12 +3,20 @@ import { transliterator } from '../utility/transliterators';
 
 export default function TextOutPut (props) {
 
+  let scanText = props.currentScanner;
+
+  const toggleScanner = () => {
+    props.toggleScanner();
+  };
+
   const toProcess = props.currentState;
-  const output = transliterator(toProcess);
+  const transliterated = transliterator(toProcess);
+  const scanned = 'scan text!';
 
   return (
     <div>
-      <pre className='output' placeholder='See Greek script appear here' rows='20'> {output} </pre>
+      <pre className='output' rows='20'> {scanText ? scanned : transliterated} </pre>
+      <button type='submit' className='btn' onClick={ toggleScanner }>Toggle Scanner</button>
     </div>
   )
 }
