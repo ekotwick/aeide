c9f4930209eb9eee928a01960280657f616ce5b5
ekotwick <edmond.kotwick@gmail.com>
2017-04-29 11:46:19 -0400

    closes #8

---
+++ app/components/TextInput.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function TextInput (props) {
+
+  const textEnter = e => {
+    console.log(e.target.value);
+  }
+
+  const handleSubmit = () => {
+    console.log('submit handled');
+  }
+
+  return (
+    <div>
+      <form className='form-vertical' onSubmit={ handleSubmit }>
+        <fieldset>
+          <textarea className='form-control' rows='20' placeholder='Enter transliterated Greek here' onChange={ textEnter } />
+          <button type="submit" className="btn btn-success">
+            Save Session
+          </button>
+        </fieldset>
+      </form>
+    </div>
+  )
+}
