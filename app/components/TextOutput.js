6b6e9458d14b59c08e4968fcd4e794d582ddfe4f
ekotwick <edmond.kotwick@gmail.com>
2017-04-29 21:46:43 -0400

    closes #36

--- app/components/TextOutput.js
+++ app/components/TextOutput.js
@@ -6,27 +6,32 @@ import Paper from 'material-ui/Paper';
 export default function TextOutPut (props) {
 
   const paperStyle = {
-    height: 400,
+    height: 1000,
   }
 
   const transliterated = props.currentTransliterated;
   let scanText = props.currentScanner;
   const syllables = props.syllables;
 
-  const toggleScanner = () => {
-    props.toggleScanner();
-    const builtSyllables = buildSyllables(transliterated);
-    props.setSyllables(builtSyllables);
-    console.log('\n\nset syllables called\n\n')
-    console.log(props.currentTransliterated)
-  };
+  // const toggleScanner = () => {
+  //   props.toggleScanner();
+  //   const builtSyllables = buildSyllables(transliterated);
+  //   props.setSyllables(builtSyllables);
+  //   console.log('\n\nset syllables called\n\n')
+  //   console.log(props.currentTransliterated)
+  // };
 
   return (
     <div>
       <Paper style={paperStyle} zDepth={3}>
         <pre className='output' rows='20'> {scanText ? syllables : transliterated } </pre>
-        <button type='submit' className='btn' onClick={ toggleScanner }>Toggle Scanner</button>
       </Paper>
     </div>
   )
 }
+
+
+// pass down to section sidebare:
+// currentTransliterated
+// setSyllables
+// toggle scanner
