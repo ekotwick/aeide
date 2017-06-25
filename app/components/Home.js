6a586f3248e1c8998b29b06ed54a52a36f7d78ca
ekotwick <edmond.kotwick@gmail.com>
2017-06-25 13:38:13 -0400

    integrate Header component; group components with react-bootstrap

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,20 +2,38 @@
 
 import React from 'react';
 
+import { Col } from 'react-bootstrap';
+
 import Info from './Info';
 import Text from './Text'; 
+import Header from './Header';
 import Footer from './Footer';
 
 const Home = () => {
   return (
     <div className='home'>
-      <div className='header'>
-        <h3 className='header-left'>AEIDE</h3>
-        <h4 className='header-right'>An App for Ancient Greek Poetry</h4>
-      </div>
-      <Info />
-      <Text />
-      <Footer />
+      {/*
+        Header component
+      */}
+      <Header />
+      {/*
+        Info and Text components rendered in the same row
+      */}
+      <Col lg={12}>
+        <Col lg={3}>
+          <Info />
+        </Col>
+        <Col lg={9}>
+          <Text />
+        </Col>
+      </Col>
+      {/*
+        Footer component rendered beneath Info and Text components; with text necessary to render page
+      */}
+      <Col lg{3} lgOffset={3}>
+        <Footer />
+        <h6>2017</h6>
+      </Col>
     </div>
   )
 }
