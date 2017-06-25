aa6d6ad06defc1652c09026687ebe2423a7ecd6b
ekotwick <edmond.kotwick@gmail.com>
2017-06-25 13:36:08 -0400

    create header component

---
+++ app/components/Header.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import React from 'react';
+
+const Header = () => {
+
+  return (
+    <div className='header'>
+      <h3 className='header-left'>AEIDE</h3>
+      <h4 className='header-right'>An App for Ancient Greek Poetry</h4>
+    </div>
+  )
+}
+
+export default Header;
