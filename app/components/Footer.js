56d297d36262062d308e59e53bc77a3d2a4d7535
ekotwick <edmond.kotwick@gmail.com>
2017-06-25 12:30:19 -0400

    add Footer component and nest wihtin Home component

---
+++ app/components/Footer.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+      <div>
+      <p className='dev'>App by E.Kotwick</p>
+      <ul>
+        <li>LinkedIn</li>
+        <li>GitHub</li>
+      </ul> 
+    </div>
+  )
+}
+
+export default Footer;
--- app/components/Home.js
+++ app/components/Home.js
@@ -4,26 +4,18 @@ import React from 'react';
 
 import Info from './Info';
 import Text from './Text'; 
+import Footer from './Footer';
 
-// const Home = () => (
-//   <div className='home'>
-//     <Info />
-//     <Text />
-//   </div>
-// );
 const Home = () => {
   return (
     <div className='home'>
       <div className='header'>
-        <h3 className='name'>AEIDE</h3>
-        <p className='dev'>App by E.Kotwick</p>
-        <ul>
-          <li>LinkedIn</li>
-          <li>GitHub</li>
-        </ul> 
+        <h3 className='header-left'>AEIDE</h3>
+        <h4 className='header-right'>An App for Ancient Greek Poetry</h4>
       </div>
       <Info />
       <Text />
+      <Footer />
     </div>
   )
 }
