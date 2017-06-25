9c3a60c00d2920e16bd6ab1b4ca2946de3a23771
ekotwick <edmond.kotwick@gmail.com>
2017-06-25 10:53:07 -0400

    create new sidebar/info component

---
+++ app/components/Info.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import React from 'react';
+import { connect } from 'react-redux';
+
+import ListButton from './ListButton';
+
+const Info = () => {
+
+  const aboutApp = () => null;
+
+  const aboutHexameter = () => null;
+
+  return (
+    <div className='info'>
+      <ul className='menu'>
+        <ListButton 
+          title="About this app"
+          cb={aboutApp}
+        />
+        <ListButton 
+          title="About Greek hexameter"
+          cb={aboutHexameter}
+        />
+      </ul>
+    </div>
+  )
+};
+
+export default Info;
