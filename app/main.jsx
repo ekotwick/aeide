a97accf4ff74d4508d3bcf10bae5bb19c63e221d
ekotwick <edmond.kotwick@gmail.com>
2017-06-25 10:52:52 -0400

    revise main.jsx for new components

--- app/main.jsx
+++ app/main.jsx
@@ -2,19 +2,14 @@
 import React from 'react';
 import {render} from 'react-dom';
 import { Provider } from 'react-redux';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import store from './store';
 import Routes from './routes';
 
 const Main = () => (
-  <MuiThemeProvider>
     <Provider store={store}>
       <Routes />
     </Provider>
-  </MuiThemeProvider>
 )
 
 render (
