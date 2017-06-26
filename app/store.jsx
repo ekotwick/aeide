9a03b662388d6beab106916fcf89d9f07575a760
ekotwick <edmond.kotwick@gmail.com>
2017-06-26 07:54:13 -0400

    refactor and add composeWithDevTools middelware

--- app/store.jsx
+++ app/store.jsx
@@ -1,6 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './reducers';
-import createLogger from 'redux-logger'; // https://github.com/evgenyrodionov/redux-logger
-import thunkMiddleware from 'redux-thunk'; // https://github.com/gaearon/redux-thunk
+import rootReducer from './reducers/index';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-export default createStore(rootReducer, applyMiddleware(thunkMiddleware, createLogger()))
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(
+      thunkMiddleware
+    )
+  )
+)
+
+export default store;
