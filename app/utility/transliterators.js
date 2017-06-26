69540be7437b09fd111c5c480f3fdd138e4bc236
ekotwick <edmond.kotwick@gmail.com>
2017-06-26 07:51:17 -0400

    rename "utility" directory

--- app/utility/transliterators.js
+++
@@ -1,7 +0,0 @@
-import { engToGrk } from './dictionary';
-
-export const transliterator = str => {
-  return str.split('\n').map(line => {
-    return line.split('').map(ch => engToGrk[ch]).join('');
-  }).join('\n');
-};
