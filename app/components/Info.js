2836a12e2878f6e480986a9fabee9f3fd0e95596
ekotwick <edmond.kotwick@gmail.com>
2017-06-26 17:10:37 -0400

    add react skylight popups to footer

--- app/components/Info.js
+++ app/components/Info.js
@@ -2,40 +2,95 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
+import SkyLight from 'react-skylight';
 
 import { Row } from 'react-bootstrap';
 
 import ListButton from './ListButton';
+import { AboutApp, AboutHexameter, ComingSoon } from './SkyLightText';
 
-const Info = () => {
+class Info extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const aboutApp = () => null;
+    this.aboutApp = this.aboutApp.bind(this);
+    this.aboutHexameter = this.aboutHexameter.bind(this);
+    this.comingSoon = this.comingSoon.bind(this);
+  }
 
-  const aboutHexameter = () => null;
+  aboutApp() {
+    this.refs.aboutApp.show();
 
-  const comingSoon = () => null;
+    this.refs.aboutHexameter.hide();
+    this.refs.soon.hide();
+  }
 
-  return (
-    <div className='info'>
-      <ul className='vert-buttons'>
-        <ListButton
-          className='vert-button'
-          title="About this app"
-          cb={aboutApp}
-        />
-        <ListButton
-          className='vert-button'
-          title="About Greek hexameter"
-          cb={aboutHexameter}
-        />
-        <ListButton
-          className='vert-button'
-          title="Coming soon"
-          cb={comingSoon}
-        />
-      </ul>
-    </div>
-  )
+  aboutHexameter() {
+    this.refs.aboutHexameter.show();
+
+    this.refs.aboutApp.hide();
+    this.refs.soon.hide();
+  }
+
+  comingSoon() {
+    this.refs.soon.show();
+
+    this.refs.aboutApp.hide();
+    this.refs.aboutHexameter.hide();
+  }
+
+  render() {
+    return (
+      <div className='info'>
+        <ul className='vert-buttons'>
+          <ListButton
+            className='vert-button'
+            title="About this app"
+            cb={this.aboutApp}
+          />
+          <ListButton
+            className='vert-button'
+            title="About Greek hexameter"
+            cb={this.aboutHexameter}
+          />
+          <ListButton
+            className='vert-button'
+            title="Coming soon"
+            cb={this.comingSoon}
+          />
+        </ul>
+          <SkyLight className='skylight' dialogStyles={styles.myDialog} hideOnOverlayClicked ref='aboutApp'>
+            <AboutApp />
+          </SkyLight>
+          <SkyLight className='skylight' dialogStyles={styles.myDialog} hideOnOverlayClicked ref='aboutHexameter'>
+            <AboutHexameter />
+          </SkyLight>
+          <SkyLight className='skylight' dialogStyles={styles.myDialog} hideOnOverlayClicked ref='soon'>
+            <ComingSoon />
+          </SkyLight>
+      </div>
+    )
+  }
 };
 
 export default Info;
+
+const styles = {
+  myDialog: {
+    backgroundColor: 'rgba(50, 0, 96, 0.9)',
+    color: '#ffffff',
+    width: '85%',
+    height: '85%',
+    fontSize: '1.5em',
+    padding: '2em',
+    position: 'fixed',
+    zIndex: 500,
+    lineHeight: '2em',
+    // vertical alignment
+    top:0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    margin: 'auto',
+  }
+};
